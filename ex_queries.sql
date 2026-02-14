f3124a36cbfbb3b2dc6bf8a6b72b9eca3ca10bb6
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-02-14 01:34:56 +0100

    feat: JOIN queries over

--- ex_queries.sql
+++ ex_queries.sql
@@ -76,6 +76,92 @@ ON degrees.department_id = departments.id
 GROUP BY departments.id;
 
 
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+SELECT students.id, students.name, students.surname, degrees.id AS deg_id, degrees.name AS degree_name
+FROM students
+INNER JOIN degrees
+ON degrees.id = students.degree_id
+WHERE degrees.name LIKE "%Laurea in Econo%";
+
+
+-- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+SELECT degrees.id, degrees.name as degree_name, degrees.level, departments.name as department
+FROM degrees
+INNER JOIN departments
+ON degrees.department_id = departments.id
+WHERE departments.name LIKE "%Neuroscienze"
+AND degrees.level = "magistrale";
+
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+SELECT courses.id, courses.name as course_name, courses.period, courses.year, teachers.name as teacher, teachers.surname
+FROM courses
+INNER JOIN course_teacher
+ON course_teacher.course_id = courses.id
+INNER JOIN teachers
+ON teachers.id = course_teacher.teacher_id
+WHERE teachers.id = 44;
+
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e 
+-- il relativo dipartimento, in ordine alfabetico per cognome e nome
+SELECT students.id, students.name, students.surname, degrees.name as degree, departments.name as department
+FROM students
+INNER JOIN degrees
+ON degrees.id = students.degree_id
+INNER JOIN departments
+ON departments.id = degrees.department_id
+ORDER BY students.surname, students.name;
+
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+SELECT degrees.id, degrees.name as degree, degrees.level, courses.name as course, courses.year, teachers.name as teacher, teachers.surname
+FROM degrees
+INNER JOIN courses
+ON courses.degree_id = degrees.id
+INNER JOIN course_teacher
+ON courses.id = course_teacher.course_id
+INNER JOIN teachers
+ON teachers.id = course_teacher.teacher_id
+ORDER BY degrees.id;
+
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+SELECT teachers.id, teachers.name, teachers.surname
+FROM teachers
+INNER JOIN course_teacher
+ON course_teacher.teacher_id = teachers.id
+INNER JOIN courses
+ON courses.id = course_teacher.course_id
+INNER JOIN degrees
+ON degrees.id = courses.degree_id
+INNER JOIN departments
+ON departments.id = degrees.department_id
+WHERE departments.name LIKE "%Matematica"
+GROUP BY teachers.id;
+
+
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT students.id, students.name, students.surname, exams.course_id, count(exam_student.exam_id) as tentativi, max(exam_student.vote) as voto_finale
+FROM students
+INNER JOIN exam_student
+ON exam_student.student_id = students.id
+INNER JOIN exams
+ON exams.id = exam_student.exam_id
+GROUP BY students.id, exams.course_id
+HAVING voto_finale > 17;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
